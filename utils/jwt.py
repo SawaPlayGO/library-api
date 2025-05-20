47486c40cba1bdd7b22e65f2230feef412158c34
SawaPlayGO <sawaglumov2006@mail.ru>
2025-05-20 19:56:43 +0700

    feat: функция проверки валидности токена

--- utils/jwt.py
+++ utils/jwt.py
@@ -1,4 +1,6 @@
 from jose import jwt
+from config import settings
+
 
 class JWT:
     """Класс для работы с JWT токенами"""
@@ -16,3 +18,15 @@ class JWT:
         """
         assert payload, 'Payload для токена не может быть пустым'
         return jwt.encode(payload, self.secret_key, algorithm=self.algorithm)
+    
+    def verify_token(self, token: str) -> dict:
+        """
+        Проверка JWT токена
+
+        :param token: JWT токен
+
+        :return: словарь с данными из токена
+        """
+        return jwt.decode(token, self.secret_key, algorithms=self.algorithm)
+    
+jwt_handler = JWT(secret_key=settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
