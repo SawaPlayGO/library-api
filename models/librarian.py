139dbf76e94b192ccbf2df1c9e74974da0e90b16
SawaPlayGO <sawaglumov2006@mail.ru>
2025-05-21 17:35:46 +0700

    delete: удаление моделей которые не были использованы

--- models/librarian.py
+++
@@ -1,6 +0,0 @@
-class Librarian:
-    """Класс для работы с библиотекарями"""
-    def __init__(self, id: int, email: str, password: str):
-        self.id = id
-        self.email = email
-        self.password = password
--- models/reader.py
+++
@@ -1,8 +0,0 @@
-class Reader:
-    """Класс для работы с читателями"""
-    def __init__(self, id: int, email: str, password: str):
-        self.id = id
-        self.email = email
-        self.password = password
-
-    
