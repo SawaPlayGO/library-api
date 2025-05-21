3a0cf6cd1a7bd1cff21ec5376454ac70eb3e5130
SawaPlayGO <sawaglumov2006@mail.ru>
2025-05-21 16:35:17 +0700

    delete: удалён параметр JWT_ACCESS_TOKEN_EXPIRE_MINUTES токен живёт с момента его создания

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ class Settings:
         # JWT
         self.JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret")
         self.JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-        self.JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
         # БД
         self.DB_HOST = os.getenv("DB_HOST", "localhost")
